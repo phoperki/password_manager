c6771f13256ca740c2607a030ce5075e2350c6f1
Phoenix <phoenix@phoenixperkins.com>
2025-10-23 20:04:28 -0500

    added password strength checker

--- password_manager.py
+++ password_manager.py
@@ -1,15 +1,7 @@
 import csv
-# 1. Store account credentials
-field_names=['account', 'username', 'password']
-def add_account(account, username, password):
-    try:
-        with open("accounts.csv", "a", newline="") as f:
-            accounts_writer = csv.DictWriter(f, field_names)
-            accounts_writer.writerow({'account': account, 'username' : username, 'password' : password})
-    except:
-        pass
 
-# 2. View Stored Accounts
+
+# 1. View Stored Accounts
 def read_accounts():
     try:
         with open("accounts.csv", "r", newline="") as f:
@@ -20,16 +12,74 @@ def read_accounts():
         with open("accounts.csv", "w", newline="") as f:
             accounts_writer = csv.DictWriter(f, field_names)
             accounts_writer.writeheader()
-            accounts_writer.writerow({'account': 'default', 'username' : 'default', 'password' : 'password1234'})
+            accounts_writer.writerow({'account': 'default', 'username' : 'default', 'password' : 'password1234', 'strength' : 'weak'})
     except PermissionError:
         print("You do not have the proper permissions for this file.")
 
 
+# 2. Store account credentials
+field_names=['account', 'username', 'password', 'strength']
+def add_account(account, username, password):
+    try:
+        
+        strength = check_password_strength(password)
+
+        if strength == "Weak":
+            print(f"WARNING {password} is a weak password.")
+            change = input("Do you want to change it? ")
+            if change == "Yes":
+                password = input("What is your new password: ")
+            elif change == "No" or change == "":
+                password = password
+        elif strength == "Medium":
+            print("This password has medium strength")
+        elif strength == "Strong":
+            print("Good choice of password. This is strong!")
+        
+        
+        with open("accounts.csv", "a", newline="") as f:
+            accounts_writer = csv.DictWriter(f, field_names)
+            accounts_writer.writerow({'account': account, 'username' : username, 'password' : password, 'strength' : strength})
+
+
+    except:
+        pass
+
+
 def show_accounts():
     accounts=read_accounts()
-    print("Account | Username | Password")
+    print("Account | Username | Password | Strength")
     for row in accounts:
-        print(f"{row['account']} | {row['username']} | {row['password']}")
+        print(f"{row['account']} | {row['username']} | {row['password']} | {row['strength']}")
+    
+
+def check_password_strength(password):
+    checks=0
+    # Check password length
+    passlength = len(password)
+
+    #Create check for uppercase
+    if any(char.isupper() for char in password):
+        checks=checks+1
+    #Create check for lowercase
+    if any(char.islower() for char in password):
+        checks=checks+1
+    #Create check for number
+    if any(char.isdigit() for char in password):
+        checks=checks+1
+    #Create check for special character
+    if any(not char.isalnum() for char in password):
+        checks=checks+1
+
+    # If password length >= 12 has uppercase, lowercase, number, and special character = strong
+    if passlength >=12 and checks==4:
+        return("Strong")
+    # If password length >= 8 has atleast 3 
+    elif passlength >= 8 & checks>=3:
+        return("Medium")
+    # Else false
+    else:
+        return("Weak")
     
 
 # 3. Add a menu
