ad4ab95abd962941956b27e4caf3388d0f0215df
Phoenix <phoenix@phoenixperkins.com>
2025-10-24 19:52:18 -0500

    v0.3 complete. Added searching for accounts

--- password_manager.py
+++ password_manager.py
@@ -6,44 +6,50 @@ def read_accounts():
     try:
         with open("accounts.csv", "r", newline="") as f:
             accounts_reader = csv.DictReader(f)
-            accounts = accounts_reader
-            return list(accounts)
+            return list(accounts_reader)
+        
     except FileNotFoundError:
         with open("accounts.csv", "w", newline="") as f:
             accounts_writer = csv.DictWriter(f, field_names)
             accounts_writer.writeheader()
             accounts_writer.writerow({'account': 'default', 'username' : 'default', 'password' : 'password1234', 'strength' : 'weak'})
+            return []
+        
     except PermissionError:
         print("You do not have the proper permissions for this file.")
+        return []
 
 
 # 2. Store account credentials
 field_names=['account', 'username', 'password', 'strength']
+
 def add_account(account, username, password):
     try:
         
         strength = check_password_strength(password)
-
+        
         if strength == "Weak":
-            print(f"WARNING {password} is a weak password.")
-            change = input("Do you want to change it? ")
-            if change == "Yes":
-                password = input("What is your new password: ")
-            elif change == "No" or change == "":
-                password = password
+            while strength == "Weak":
+                print(f"WARNING {password} is a weak password.")
+                change = input("Do you want to change it? (yes/no)").lower()
+                if change == "yes":
+                    password = input("What is your new password: ")
+                    strength = check_password_strength(password)
+                elif change == "no":
+                    break
+
         elif strength == "Medium":
             print("This password has medium strength")
+
         elif strength == "Strong":
             print("Good choice of password. This is strong!")
-        
-        
+                
         with open("accounts.csv", "a", newline="") as f:
             accounts_writer = csv.DictWriter(f, field_names)
             accounts_writer.writerow({'account': account, 'username' : username, 'password' : password, 'strength' : strength})
 
-
-    except:
-        pass
+    except Exception as e:
+        print(f"Failed to add account: {e}")
 
 
 def show_accounts():
@@ -70,7 +76,6 @@ def check_password_strength(password):
     #Create check for special character
     if any(not char.isalnum() for char in password):
         checks=checks+1
-
     # If password length >= 12 has uppercase, lowercase, number, and special character = strong
     if passlength >=12 and checks==4:
         return("Strong")
@@ -81,6 +86,15 @@ def check_password_strength(password):
     else:
         return("Weak")
     
+def search_for_account(account):
+    accounts_list = read_accounts()
+
+    for account_dict in accounts_list:
+        if account.lower() == account_dict['account'].lower():
+            return account_dict['password']
+        
+    return "Account not found"
+
 
 # 3. Add a menu
 def menu():
@@ -89,8 +103,10 @@ def menu():
         print("Make your selection: \n" \
         "1. View accounts \n" \
         "2. Add account \n" \
-        "3. Quit")
+        "3. Search for Account \n" \
+        "4. Quit")
         x = int(input("What is your choice: "))
+        print("-------------")
         if x == 1:
             show_accounts()
         elif x == 2:
@@ -99,7 +115,11 @@ def menu():
             password = input("What is the password: ")
             add_account(account, username, password)
         elif x == 3:
+            account = input("What is the account: ")
+            password = search_for_account(account)
+            print(f"Account: {account} | Password: {password}")
+        elif x == 4:
             print("Thank you for securing your passwords. Goodbye!")
             break
-
+        print("-------------")
 menu()
